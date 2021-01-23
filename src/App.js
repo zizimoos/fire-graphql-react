a24d64f5f2fbb4655f9dc258a99386d009343e1b
zizimoos_iMac <zizimoos@gmail.com>
2021-01-24 02:04:17 +0900

    apollo client init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import { Detail } from "./routes/Detail";
+import { Home } from "./routes/Home";
+
 function App() {
-  return <div>HELLO WORLD</div>;
+  return (
+    <Router>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/:id" component={Detail}></Route>
+    </Router>
+  );
 }
 
 export default App;
